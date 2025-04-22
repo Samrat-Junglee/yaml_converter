4d4b8271506f6526117eb2cf998bd0c854efafe9
Samrat Mukherjee <samrat.mukherjee@jungleegames.com>
2025-04-22 18:37:44 +0530

    made required: false

--- lib/creator_page.dart
+++ lib/creator_page.dart
@@ -19,43 +19,55 @@ class _CreatorPageState extends State<CreatorPage> {
 
   /// Converts a given JSON string into a YAML representation for form configuration.
   /// The resulting YAML format includes fields, labels, descriptions, types, and validations.
-  /// 
+  ///
   /// The `handleJson` function recursively processes the JSON data and generates
   /// corresponding YAML content for objects, arrays, and primitive types.
-  /// 
+  ///
   /// Parameters:
   /// - [json]: A JSON string to be converted.
   /// - [indentationSpace]: The current indentation level used for YAML formatting.
-  /// 
+  ///
   /// Example Usage:
-  /// 
+  ///
   /// String json = '{"name": "John", "age": 30, "isAdmin": false}';
   /// handleJson(json, 2);
-  /// 
+  ///
   void handleJson(String json, int indentationSpace) {
     Map<String, dynamic> jsonData = jsonDecode(json);
-    
+
     for (int i = 0; i < jsonData.entries.length; i++) {
       if (jsonData.entries.toList()[i].value is Map<String, dynamic>) {
         yamlText += Utils.addSpaces("- id: ${Utils.handleNumericValueForYaml(jsonData.entries.toList()[i].key)}", indentationSpace) + "\n";
-        yamlText += Utils.addSpaces("label: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) + "\n";
-        yamlText += Utils.addSpaces("description: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) + "\n";
+        yamlText += Utils.addSpaces(
+                "label: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) +
+            "\n";
+        yamlText += Utils.addSpaces(
+                "description: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) +
+            "\n";
         yamlText += Utils.addSpaces("type: object", indentationSpace + 2) + "\n";
         yamlText += Utils.addSpaces("fields:", indentationSpace + 2) + "\n";
         handleJson(jsonEncode(jsonData.entries.toList()[i].value), indentationSpace + 4);
       } else if (jsonData.entries.toList()[i].value is List<dynamic>) {
         if (jsonData.entries.toList()[i].value[0] is Map<String, dynamic>) {
           yamlText += Utils.addSpaces("- id: ${Utils.handleNumericValueForYaml(jsonData.entries.toList()[i].key)}", indentationSpace) + "\n";
-          yamlText += Utils.addSpaces("label: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) + "\n";
-          yamlText += Utils.addSpaces("description: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) + "\n";
+          yamlText += Utils.addSpaces(
+                  "label: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) +
+              "\n";
+          yamlText += Utils.addSpaces("description: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}",
+                  indentationSpace + 2) +
+              "\n";
           yamlText += Utils.addSpaces("type: objectList", indentationSpace + 2) + "\n";
           yamlText += Utils.addSpaces("objectFields:", indentationSpace + 2) + "\n";
           Map<String, dynamic> jsonValue = jsonData.entries.toList()[i].value[0];
           handleJson(jsonEncode(jsonValue), indentationSpace + 4);
         } else if (jsonData.entries.toList()[i].value[0] is int || jsonData.entries.toList()[i].value[0] is double) {
           yamlText += Utils.addSpaces("- id: ${Utils.handleNumericValueForYaml(jsonData.entries.toList()[i].key)}", indentationSpace) + "\n";
-          yamlText += Utils.addSpaces("label: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) + "\n";
-          yamlText += Utils.addSpaces("description: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) + "\n";
+          yamlText += Utils.addSpaces(
+                  "label: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) +
+              "\n";
+          yamlText += Utils.addSpaces("description: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}",
+                  indentationSpace + 2) +
+              "\n";
           yamlText += Utils.addSpaces("type: List", indentationSpace + 2) + "\n";
           if (jsonData.entries.toList()[i].value[0] is String) {
             yamlText += Utils.addSpaces("subtype: TextField", indentationSpace + 2) + "\n";
@@ -67,24 +79,36 @@ class _CreatorPageState extends State<CreatorPage> {
           yamlText += Utils.addSpaces("fields:", indentationSpace + 2) + "\n";
           yamlText += Utils.addSpaces("validations:", indentationSpace + 2) + "\n";
           yamlText += Utils.addSpaces("required: true", indentationSpace + 4) + "\n";
-          yamlText += Utils.addSpaces("pattern: ${Utils.validationPattern(jsonData.entries.toList()[i].value[0].toString())}", indentationSpace + 4) + "\n";
-          yamlText += Utils.addSpaces("message: ${Utils.validationMessage(jsonData.entries.toList()[i].value[0].toString())}", indentationSpace + 4) + "\n";
+          yamlText +=
+              Utils.addSpaces("pattern: ${Utils.validationPattern(jsonData.entries.toList()[i].value[0].toString())}", indentationSpace + 4) + "\n";
+          yamlText +=
+              Utils.addSpaces("message: ${Utils.validationMessage(jsonData.entries.toList()[i].value[0].toString())}", indentationSpace + 4) + "\n";
         } else if (jsonData.entries.toList()[i].value[0] is String) {
           yamlText += Utils.addSpaces("- id: ${Utils.handleNumericValueForYaml(jsonData.entries.toList()[i].key)}", indentationSpace) + "\n";
-          yamlText += Utils.addSpaces("label: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) + "\n";
-          yamlText += Utils.addSpaces("description: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) + "\n";
+          yamlText += Utils.addSpaces(
+                  "label: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) +
+              "\n";
+          yamlText += Utils.addSpaces("description: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}",
+                  indentationSpace + 2) +
+              "\n";
           yamlText += Utils.addSpaces("type: List", indentationSpace + 2) + "\n";
           yamlText += Utils.addSpaces("subtype: TextField", indentationSpace + 2) + "\n";
           yamlText += Utils.addSpaces("fields:", indentationSpace + 2) + "\n";
           yamlText += Utils.addSpaces("validations:", indentationSpace + 2) + "\n";
           yamlText += Utils.addSpaces("required: true", indentationSpace + 4) + "\n";
-          yamlText += Utils.addSpaces("pattern: ${Utils.validationPattern(jsonData.entries.toList()[i].value[0].toString())}", indentationSpace + 4) + "\n";
-          yamlText += Utils.addSpaces("message: ${Utils.validationMessage(jsonData.entries.toList()[i].value[0].toString())}", indentationSpace + 4) + "\n";
+          yamlText +=
+              Utils.addSpaces("pattern: ${Utils.validationPattern(jsonData.entries.toList()[i].value[0].toString())}", indentationSpace + 4) + "\n";
+          yamlText +=
+              Utils.addSpaces("message: ${Utils.validationMessage(jsonData.entries.toList()[i].value[0].toString())}", indentationSpace + 4) + "\n";
         }
       } else {
         yamlText += Utils.addSpaces("- id: ${Utils.handleNumericValueForYaml(jsonData.entries.toList()[i].key)}", indentationSpace) + "\n";
-        yamlText += Utils.addSpaces("label: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) + "\n";
-        yamlText += Utils.addSpaces("description: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) + "\n";
+        yamlText += Utils.addSpaces(
+                "label: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) +
+            "\n";
+        yamlText += Utils.addSpaces(
+                "description: ${Utils.handleNumericValueForYaml(Utils.camelCaseToEnglish(jsonData.entries.toList()[i].key))}", indentationSpace + 2) +
+            "\n";
         if (jsonData.entries.toList()[i].value is String) {
           yamlText += Utils.addSpaces("type: TextField", indentationSpace + 2) + "\n";
         } else if (jsonData.entries.toList()[i].value is int || jsonData.entries.toList()[i].value is double) {
@@ -96,9 +120,11 @@ class _CreatorPageState extends State<CreatorPage> {
           // No validations required
         } else {
           yamlText += Utils.addSpaces("validations:", indentationSpace + 2) + "\n";
-          yamlText += Utils.addSpaces("required: true", indentationSpace + 4) + "\n";
-          yamlText += Utils.addSpaces("pattern: ${Utils.validationPattern(jsonData.entries.toList()[i].value.toString())}", indentationSpace + 4) + "\n";
-          yamlText += Utils.addSpaces("message: ${Utils.validationMessage(jsonData.entries.toList()[i].value.toString())}", indentationSpace + 4) + "\n";
+          yamlText += Utils.addSpaces("required: false", indentationSpace + 4) + "\n";
+          yamlText +=
+              Utils.addSpaces("pattern: ${Utils.validationPattern(jsonData.entries.toList()[i].value.toString())}", indentationSpace + 4) + "\n";
+          yamlText +=
+              Utils.addSpaces("message: ${Utils.validationMessage(jsonData.entries.toList()[i].value.toString())}", indentationSpace + 4) + "\n";
         }
         print("${jsonData.entries.toList()[i].key} : ${jsonData.entries.toList()[i].value} -> ${jsonData.entries.toList()[i].value.runtimeType}");
       }
@@ -107,8 +133,7 @@ class _CreatorPageState extends State<CreatorPage> {
     setState(() {
       _yamlController.text = yamlText;
     });
-  }  
-
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -119,9 +144,16 @@ class _CreatorPageState extends State<CreatorPage> {
         width: size.width,
         child: Column(
           children: [
-            SizedBox(height: 10,),
-            Text("JGC Yaml Creator", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "JGC Yaml Creator",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -129,24 +161,17 @@ class _CreatorPageState extends State<CreatorPage> {
                   children: [
                     SingleChildScrollView(
                       child: Container(
-                        margin: EdgeInsets.all(8),
-                        padding: EdgeInsets.all(16),
-                        width: size.width / 2.2,
-                        height: size.height / 1.2,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: Colors.black26),
-                          borderRadius: BorderRadius.circular(10)
-                        ),
-                        child: TextField(
-                          controller: _jsonController,
-                          maxLines: null,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Paste your json text over here"
-                          ),
-                        )
-                      ),
+                          margin: EdgeInsets.all(8),
+                          padding: EdgeInsets.all(16),
+                          width: size.width / 2.2,
+                          height: size.height / 1.2,
+                          decoration:
+                              BoxDecoration(color: Colors.white, border: Border.all(color: Colors.black26), borderRadius: BorderRadius.circular(10)),
+                          child: TextField(
+                            controller: _jsonController,
+                            maxLines: null,
+                            decoration: InputDecoration(border: InputBorder.none, hintText: "Paste your json text over here"),
+                          )),
                     ),
                     SizedBox(
                       height: 40,
@@ -156,7 +181,10 @@ class _CreatorPageState extends State<CreatorPage> {
                           handleJson(_jsonController.text, 2);
                         },
                         color: Colors.black,
-                        child: Text("Generate", style: TextStyle(color: Colors.white),),
+                        child: Text(
+                          "Generate",
+                          style: TextStyle(color: Colors.white),
+                        ),
                       ),
                     )
                   ],
@@ -176,23 +204,19 @@ class _CreatorPageState extends State<CreatorPage> {
                   children: [
                     SingleChildScrollView(
                       child: Container(
-                        margin: EdgeInsets.all(8),
-                        padding: EdgeInsets.all(16),
-                        width: size.width / 2.2,
-                        height: size.height / 1.2,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(color: Colors.black26),
-                          borderRadius: BorderRadius.circular(10)
-                        ),
-                        child: TextField(
-                          controller: _yamlController,
-                          maxLines: null,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                          ),
-                        )
-                      ),
+                          margin: EdgeInsets.all(8),
+                          padding: EdgeInsets.all(16),
+                          width: size.width / 2.2,
+                          height: size.height / 1.2,
+                          decoration:
+                              BoxDecoration(color: Colors.white, border: Border.all(color: Colors.black26), borderRadius: BorderRadius.circular(10)),
+                          child: TextField(
+                            controller: _yamlController,
+                            maxLines: null,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                            ),
+                          )),
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -208,10 +232,15 @@ class _CreatorPageState extends State<CreatorPage> {
                               });
                             },
                             color: Colors.red,
-                            child: Text("Clear Yaml", style: TextStyle(color: Colors.white),),
+                            child: Text(
+                              "Clear Yaml",
+                              style: TextStyle(color: Colors.white),
+                            ),
                           ),
                         ),
-                        SizedBox(width: 10,),
+                        SizedBox(
+                          width: 10,
+                        ),
                         SizedBox(
                           height: 40,
                           width: size.width / 4.5,
@@ -220,7 +249,10 @@ class _CreatorPageState extends State<CreatorPage> {
                               Clipboard.setData(ClipboardData(text: _yamlController.text));
                             },
                             color: Colors.black,
-                            child: Text("Copy Yaml", style: TextStyle(color: Colors.white),),
+                            child: Text(
+                              "Copy Yaml",
+                              style: TextStyle(color: Colors.white),
+                            ),
                           ),
                         ),
                       ],
